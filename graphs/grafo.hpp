4041bfc905c0bc4c9af3ac43e4cdb8e3aba3b422
Eugenio Alves <eaof@icomp.ufam.edu.br>
2024-05-02 11:10:12 -0400

    using vector

--- graphs/grafo.hpp
+++ graphs/grafo.hpp
@@ -2,8 +2,27 @@
 #define grafo
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+class Vertice{
+    int id;
+};
+
+
+class ListaNo{
+    public:
+    int dist;
+
+    ListaNo(){
+        dist = 0;
+    }
+
+    ListaNo(int num){
+        dist = num;
+    }
+};
 
 
 void hello();
--- graphs/main.cpp
+++ graphs/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include "grafo.hpp"
 
 
-int main(int argc, char argv[]){
-    
+int main(int argc, char *argv[]){
+    hi();
+    hello();
+    vector<ListaNo> enc;
+    enc.push_back(ListaNo(4));
+    enc.push_back(ListaNo(6));
+    enc.push_back(ListaNo(7));
+    enc.push_back(ListaNo(2));
+
+    for(int i=0;i < enc.size();i++){
+        cout << enc.at(i).dist << endl;
+    }
 }
